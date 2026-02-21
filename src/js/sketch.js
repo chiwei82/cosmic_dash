ab3c1b01f12b7b21b222be5a579f687c8c4e347e
Ray-red-byte <ray67672@gmail.com>
2026-02-21 13:24:30 +0000

    add : add debris.js and make random show debris

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,10 +1,20 @@
+let debrises = [];
+
 function setup() {
     createCanvas(1200, 600);
     ship = new Spaceship(100, height / 2);
+
+    for (let i = 0; i < 10; i++) {
+        debrises.push(new Debris(random(width / 2, width), random(height)));
+    }
 }
 
 function draw() {
     background(220);
     ship.updatePosition();
     ship.show();
+
+    for (let i = 0; i < debrises.length; i++) {
+        debrises[i].show();
+    }
 }
