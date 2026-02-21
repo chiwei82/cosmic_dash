1f1398cab3defeb2a856c1bddd26455a327619dd
Ray-red-byte <ray67672@gmail.com>
2026-02-21 17:35:24 +0000

    refactor

---
+++ src/game_controller.js
@@ -0,0 +1,22 @@
+class GameController {
+
+
+    init() {
+        // Initialize starting objects, like ship
+    }
+
+    update() {
+        // Update all object positions
+        // Check game ended
+        // Spawn new objects
+        // Check collisions
+    }
+
+    keyPressed(key) {
+
+    }
+
+    keyReleased(key) {
+
+    }
+}
--- src/js/sketch.js
+++ src/js/sketch.js
@@ -2,80 +2,92 @@ let debris_list = [];
 let projectile_list = [];
 
 function setup() {
-    createCanvas(1200, 600, WEBGL);
-    ship = new Spaceship(100, height / 2);
+    // createCanvas(1200, 600, WEBGL);
+    // ship = new Spaceship(100, height / 2);
 
-    bgEarth = new BackgroundEarth("assets/earth_texture.jpg", 600);
-    bgEarth.load();
+    // bgEarth = new BackgroundEarth("assets/earth_texture.jpg", 600);
+    // bgEarth.load();
 
-    for (let i = 0; i < 10; i++) {
-        debris_list.push(new SpaceItem(random(width / 2, width), random(height)));
-        spawnSpaceItem(true); // Initial spawn spread across the screen
-    }
+    // for (let i = 0; i < 10; i++) {
+    //     debris_list.push(new SpaceItem(random(width / 2, width), random(height)));
+    //     spawnSpaceItem(true); // Initial spawn spread across the screen
+    // }
+    createCanvas(1200, 600);
+
+    state = new GameState();
+    controller = new GameController(state);
+    view = new GameView(state);
+
+    controller.init();
 }
 
 function draw() {
-    background(220);
-    bgEarth.show();
-    resetMatrix();
-    translate(-width / 2, -height / 2);
+    controller.update();
+    view.draw();
 
-    ship.update(projectile_list);
-    ship.show();
+    // background(220);
+    // bgEarth.show();
+    // resetMatrix();
+    // translate(-width / 2, -height / 2);
 
-    for (let p of projectile_list) p.show();
+    // ship.update(projectile_list);
+    // ship.show();
 
-    // TODO: Refactor debris spawn logic somewhere else
-    if (frameCount % 20 === 0) {
-        spawnSpaceItem(false);
-    }
+    // for (let p of projectile_list) p.show();
 
-    // Iterate backwards when removing items from an array to prevent index shifting bugs
-    for (let i = debris_list.length - 1; i >= 0; i--) {
-        let currentDebris = debris_list[i];
+    // // TODO: Refactor debris spawn logic somewhere else
+    // if (frameCount % 20 === 0) {
+    //     spawnSpaceItem(false);
+    // }
 
-        currentDebris.updatePosition();
-        currentDebris.show();
+    // // Iterate backwards when removing items from an array to prevent index shifting bugs
+    // for (let i = debris_list.length - 1; i >= 0; i--) {
+    //     let currentDebris = debris_list[i];
 
-        // Remove if it goes off-screen or if it was destroyed/collected (isActive is false)
-        if (currentDebris.x < -30 || !currentDebris.isActive) {
-            debris_list.splice(i, 1);
-        }
-    }
+    //     currentDebris.updatePosition();
+    //     currentDebris.show();
+
+    //     // Remove if it goes off-screen or if it was destroyed/collected (isActive is false)
+    //     if (currentDebris.x < -30 || !currentDebris.isActive) {
+    //         debris_list.splice(i, 1);
+    //     }
+    // }
 
 }
 
 function keyPressed() {
-    if (key === ' ') {
-        ship.weapon.fire(projectile_list);
-    }
+    controller.keyPressed(key);
+    // if (key === ' ') {
+    //     ship.weapon.fire(projectile_list);
+    // }
 }
 
 function keyReleased() {
-    if (key === ' ') {
-        ship.weapon.stopFiring(projectile_list);
-    }
+    controller.keyReleased(key);
+    // if (key === ' ') {
+    //     ship.weapon.stopFiring(projectile_list);
+    // }
 }
-function spawnSpaceItem(isInitialSetup = false) {
-    let x = isInitialSetup ? random(width / 2, width) : width + 50;
-    let y = random(height);
+// function spawnSpaceItem(isInitialSetup = false) {
+//     let x = isInitialSetup ? random(width / 2, width) : width + 50;
+//     let y = random(height);
 
-    let size = random(15, 40);
+//     let size = random(15, 40);
 
-    // Generates a random integer between 0 and 2
-    let type = Math.floor(random(2));
-    let newDebris;
+//     // Generates a random integer between 0 and 2
+//     let type = Math.floor(random(2));
+//     let newDebris;
 
-    switch (type) {
-        case 0:
-            newDebris = new SpaceHazard(x, y, size);
-            break;
-        case 1:
-            newDebris = new SpaceJunk(x, y, size);
-            break;
-        default:
-            newDebris = new SpaceHazard(x, y,); // Safe fallback
-    }
+//     switch (type) {
+//         case 0:
+//             newDebris = new SpaceHazard(x, y, size);
+//             break;
+//         case 1:
+//             newDebris = new SpaceJunk(x, y, size);
+//             break;
+//         default:
+//             newDebris = new SpaceHazard(x, y,); // Safe fallback
+//     }
 
-    debris_list.push(newDebris);
-}
+//     debris_list.push(newDebris);
+// }
