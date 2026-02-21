30e6fc08e22756fc565c13c850a3acaa4d8da468
Alex Hoover <alexwhoover@gmail.com>
2026-02-21 13:29:43 +0000

    Added spaceship movement

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(1200, 600);
     ship = new Spaceship(100, height / 2);
 }
 
 function draw() {
     background(220);
-    // ship.updatePosition();
+    ship.updatePosition();
     ship.show();
 }
