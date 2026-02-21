980ce8d96864ad3eecf2183f2085092af3f2dc14
Alex Hoover <alexwhoover@gmail.com>
2026-02-21 13:29:43 +0000

    Added spaceship movement

--- src/js/entities/spaceship.js
+++ src/js/entities/spaceship.js
@@ -1,38 +1,57 @@
 class Spaceship {
     constructor(x, y) {
+        this.shipWidth = 40;
+        this.shipHeight = 30;
+        this.shipColor = color(255, 0, 0);
+
         this.x = x;
         this.y = y;
         this.vy = 0; // vertical velocity (pixels per frame time)
         this.ay = 0; // vertical acceleration (pixels per frame time squared)
-        this.thrust = 0.5; // Acceleration per frame when key held
+        this.thrust = 0.1; // delta acceleration per frame when key held
+        this.damping = 0.98;
     }
 
     handleInput() {
         if (keyIsDown(UP_ARROW)) {
-            this.ay = this.thrust;
+            this.ay = -this.thrust;
         }
         else if (keyIsDown(DOWN_ARROW)) {
-            this.ay = -this.thrust;
+            this.ay = this.thrust;
         }
         else {
             this.ay = 0;
         }
     }
 
+    handleWalls() {
+        if (this.y <= this.shipHeight / 2) {
+            this.y = this.shipHeight / 2;
+            this.vy = 0;
+            this.ay = 0;
+        }
+        else if (this.y >= height - this.shipHeight / 2) {
+            this.y = height - this.shipHeight / 2;
+            this.vy = 0;
+            this.ay = 0;
+        }
+    }
+
     updatePosition() {
         this.handleInput();
-        this.vy += ay // vf = vo + at, where t is frame duration and a is acceleration per frame
+        this.vy += this.ay; // vf = vo + at, where t is frame duration and a is acceleration per frame
+        this.vy *= this.damping;
         this.y += this.vy; // yf = y0 + vt, where t is frame duration
-        this.y = constrain(this.y, 0, height); // Bound spaceship to screen
+        this.handleWalls(); // Bound this.SPACESHIP to screen
     }
 
     show() {
         push();
-        fill(255, 0, 200);
+        fill(this.shipColor);
         triangle(
-            this.x + 20, this.y,
-            this.x - 20, this.y - 15,
-            this.x - 20, this.y + 15
+            this.x + this.shipWidth / 2, this.y,
+            this.x - this.shipWidth / 2, this.y - this.shipHeight / 2,
+            this.x - this.shipWidth / 2, this.y + this.shipHeight / 2
         )
         pop();
     }
